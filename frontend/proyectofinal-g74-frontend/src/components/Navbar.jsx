4256318cdc9b95bce49687ebf1a86d5962f7b702
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2025-04-14 19:58:30 -0400

    Update Navbar.jsx

--- frontend/proyectofinal-g74-frontend/src/components/Navbar.jsx
+++ frontend/proyectofinal-g74-frontend/src/components/Navbar.jsx
@@ -6,25 +6,28 @@ export default function Navbar() {
     <nav class="bg-gray-100 text-white p-4 shadow-md">
       <div class="container mx-auto flex justify-between items-center">
         <div class="flex items-center">
-          <a href="index.html" class="text-2xl font-bold">Amazzon</a>
+          <Link to="/" class="text-2xl font-bold" >
+            ZoeChalupas
+          </Link>
+
           <div class="hidden md:flex ml-10 space-x-6">
-            <a href="index.html" class="hover:text-blue-200 transition"> Inicio</a>
+            <Link to="/" class="hover:text-blue-200 transition" >
+              Inicio
+            </Link>
+            <Link to="/Gallery" class="hover:text-blue-200 transition" >
+              Galeria
+            </Link>
+            <Link to="/Login" class="hover:text-blue-200 transition" >
+              Iniciar Sesion
+            </Link>
+            {/* <a href="index.html" class="hover:text-blue-200 transition"> Inicio</a>
             <a href="gallery.html" class="hover:text-blue-200 transition"> Explorar</a>
-            <a href="#" class="hover:text-blue-200 transition"> Ofertas</a>
+            <a href="#" class="hover:text-blue-200 transition"> Ofertas</a> */}
           </div>
         </div>
       </div>
-      <div>
-        <Link to="/" style={{ color: "white", textDecoration: "none", fontSize: "24px" }}>
-          Home
-        </Link>
-      </div>
-      <div>
-        <Link to="/about" style={{ color: "white", textDecoration: "none", fontSize: "24px" }}>
-          About
-        </Link>
-      </div>
       
-      </nav>
+
+    </nav>
   );
 }
