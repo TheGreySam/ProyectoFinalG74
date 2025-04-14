12ccf4253ea5a6ae43e60e364d2fb0fa56f660a0
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2025-04-14 08:05:39 -0400

    Update Navbar.jsx

--- frontend/proyectofinal-g74-frontend/src/components/Navbar.jsx
+++ frontend/proyectofinal-g74-frontend/src/components/Navbar.jsx
@@ -3,7 +3,17 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: "1rem", background: "#f2f2f2" }}>
+    <nav class="bg-gray-100 text-white p-4 shadow-md">
+      <div class="container mx-auto flex justify-between items-center">
+        <div class="flex items-center">
+          <a href="index.html" class="text-2xl font-bold">Amazzon</a>
+          <div class="hidden md:flex ml-10 space-x-6">
+            <a href="index.html" class="hover:text-blue-200 transition"> Inicio</a>
+            <a href="gallery.html" class="hover:text-blue-200 transition"> Explorar</a>
+            <a href="#" class="hover:text-blue-200 transition"> Ofertas</a>
+          </div>
+        </div>
+      </div>
       <div>
         <Link to="/" style={{ color: "white", textDecoration: "none", fontSize: "24px" }}>
           Home
