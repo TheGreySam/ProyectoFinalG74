b920fc0e974c3d7c8f57b3d1bbb74b0c751da2eb
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2025-04-14 20:21:24 -0400

    Update Footer.jsx

--- frontend/proyectofinal-g74-frontend/src/components/Footer.jsx
+++ frontend/proyectofinal-g74-frontend/src/components/Footer.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <footer className="bg-blue-100 text-white p-4 shadow-md mt-10">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="flex items-center">
-                    <Link to="/" className="text-2xl font-bold">
-                    ZoeChalupas
-                    </Link>
-                </div>
-
+        <footer className="bg-gray-800 text-gray-300 py-4 mt-12">
+            <div class="container mx-auto px-4 text-center text-sm">
+            <p>&copy; 2025 Wilfred. Todos los derechos reservados.</p>
+            <div class="mt-2 space-x-4">
+                <a href="#" class="text-gray-300 hover:text-white">Términos y condiciones</a>
+                <a href="#" class="text-gray-300 hover:text-white">Política de privacidad</a>
+                <a href="#" class="text-gray-300 hover:text-white">Centro de ayuda</a>
             </div>
+        </div>
         </footer>
     );
 }
