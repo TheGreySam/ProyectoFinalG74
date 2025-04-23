dfb98f5aac6584b97280a03a9116771dddb24993
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2025-04-23 09:14:34 -0400

    cors instalado e implementado

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const authR = require("./routes/authRoutes");
 const marketplaceRouters = require("./routes/marketplaceRoutes");
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+app.use(cors());
 app.use(express.json());
 app.use(authR);
 app.use(marketplaceRouters);
